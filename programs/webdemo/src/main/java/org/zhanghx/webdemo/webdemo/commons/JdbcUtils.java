b1fb6d79d3e2742596e148ab7c64f611f2c6b6c2
huluBrother <xiangzi535@126.com>
2021-10-26 07:53:20 +0800

    [add] 创建数据库相关内容
    1. 创建项目的包结构(基于应用程序分层)
    2. 创建数据库和表，命令保存在 zhanghx.db，表名为User
    3. 创建pojo对象
    4.  添加数据库属性文件
    5. 编写数据库连接的公共方法

---
+++ programs/webdemo/src/main/java/org/zhanghx/webdemo/webdemo/commons/JdbcUtils.java
@@ -0,0 +1,54 @@
+package org.zhanghx.webdemo.webdemo.commons;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class JdbcUtils {
+
+    private static String driver;
+    private static String url;
+    private static String username;
+    private static String password;
+
+    static {
+
+        try {
+            //读取资源文件，获得数据库的连接信息
+            ResourceBundle db = ResourceBundle.getBundle("db");
+            driver = db.getString("driver");
+            url = db.getString("url");
+            username = db.getString("username");
+            password = db.getString("password");
+
+            //加载数据库驱动
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //声明一个获取数据库连接的方法
+    public static Connection getConnection() {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void closeConnection(Connection conn) {
+
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
