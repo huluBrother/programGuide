a4789b41124be8ce96e151ea8f322456eae3aeb1
huluBrother <xiangzi535@126.com>
2021-11-06 18:32:59 +0800

    【add】使用mybaits逆向工程生成mapper 文件
    1. 使用mybaits生成pojo文件
    2. 使用mybaits生成mapper文件

---
+++ programs/ssm/src/main/java/org/example/ssm/mapper/UsersMapper.java
@@ -0,0 +1,31 @@
+package org.example.ssm.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import org.example.ssm.pojo.Users;
+import org.example.ssm.pojo.UsersExample;
+
+import java.util.List;
+
+public interface UsersMapper {
+    int countByExample(UsersExample example);
+
+    int deleteByExample(UsersExample example);
+
+    int deleteByPrimaryKey(Integer userid);
+
+    int insert(Users record);
+
+    int insertSelective(Users record);
+
+    List<Users> selectByExample(UsersExample example);
+
+    Users selectByPrimaryKey(Integer userid);
+
+    int updateByExampleSelective(@Param("record") Users record, @Param("example") UsersExample example);
+
+    int updateByExample(@Param("record") Users record, @Param("example") UsersExample example);
+
+    int updateByPrimaryKeySelective(Users record);
+
+    int updateByPrimaryKey(Users record);
+}
