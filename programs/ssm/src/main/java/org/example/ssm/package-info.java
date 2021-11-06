eea6d3b37f566cb5f92e58a03e69b10f4bc5266c
huluBrother <xiangzi535@126.com>
2021-11-06 18:24:24 +0800

    【add】创建包结构

---
+++ programs/ssm/src/main/java/org/example/ssm/package-info.java
@@ -0,0 +1,12 @@
+package org.example.ssm;
+
+
+/**
+ * pojo : 存放数据库表对象
+ * mapper：mybaits映射语句
+ * service：业务层
+ * web.controlller: web层(servlet)
+ * exception:自定义异常
+ * interceptor：拦截器(fileter)
+ * aop: 切面
+ */
